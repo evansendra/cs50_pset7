66dd48c710529789a1115c65493b50ec0c34b651
Evan Sendra <code@evansendra.com>
2014-07-07 17:42:19 -0400

    added feature to sell stocks (all of them), which credits a user's cash in the db

--- public/sell.php
+++ public/sell.php
@@ -6,15 +6,47 @@
 	// check if the user already submitted the form
 	if ($_SERVER['REQUEST_METHOD'] == "POST")
 	{
-		// delete the row for user in holdings
-		// dump($_POST);
-
 		if ( empty($_POST["to_sell"]) )
 			apologize("Please choose one of your stocks to be sold.");
 		else
 		{
 			// delete row in holdings where id = sesh_id and stock = $_POST['to_sell']
-			// update user's new cash amount in users table
+			$stock_to_sell = $_POST["to_sell"];
+
+			$res = query("SELECT * FROM holdings WHERE id = ? AND stock = ?", 
+				$_SESSION["id"], $stock_to_sell);
+
+			
+			if ($res === false)
+				apologize("We're sorry; we couldn't sell " . $stock_to_sell . ".");
+			else if ( empty($res) )
+				apologize("It seems you don't have any " . $stock_to_sell . ".");
+
+			// obtain amount of cash to give from the sale
+			$stock_info = lookup( $res[0]["stock"] );
+
+			$cash_made = $stock_info["price"] * $res[0]["shares"];
+
+			// credit the user's account with that cash
+			query("UPDATE users SET cash = cash + ? WHERE id = ?",
+					$cash_made, $_SESSION["id"]);
+
+			query("DELETE FROM holdings WHERE id = ? AND stock = ?", 
+				$_SESSION["id"], $stock_to_sell);	
+
+			// get remaining stocks after sale
+			$stocks = query("SELECT * FROM holdings WHERE id = ?", $_SESSION["id"]);
+
+			// repopulate the sell page with success message and remaining stocks
+			$params = 
+			[
+				"stocks" => $stocks,
+				"sold_stock" => $stock_to_sell,
+				"cash_made" => $cash_made,
+				"title" => "Sell Stock"
+			];
+
+			render("sell_form.php", $params);
 		}
 	}
 	else
@@ -27,6 +59,7 @@
 
 		$params = 
 		[
+			"sold_stock" => false,
 			"stocks" => $stocks,
 			"title" => "Sell Stock"
 		];
--- templates/sell_form.php
+++ templates/sell_form.php
@@ -1,10 +1,18 @@
 <h2>Sell all the shares of one of your stocks.</h2>
+
+<?php if ($sold_stock): ?>
+    <p class="bg-success notification" onclick="$(this).hide()">
+        You sold all your <?= $sold_stock ?> for $<?= number_format($cash_made, 2)?> <br />
+        <span class="small">(click to dismiss)</span>
+    </p>
+<?php endif ?>
+
 <form action="sell.php" method="post">
     <fieldset>
         <div class="form-group">
             <select name="to_sell" id="stock_select" class="form-control">
 
-            <option value="">Choose stock</option>
+            <option value="" selected>Choose stock</option>
             <?php foreach ($stocks as $stock): ?>
 
             <?php $symbol = $stock["stock"]; ?>
